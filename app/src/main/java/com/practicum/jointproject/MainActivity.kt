a7f40db457b3afade8cc4dc61787629c7605e5cd
OlegKostinDS <olegkostinds@gmail.com>
2023-02-04 12:44:57 +0300

    day night

--- app/src/main/java/com/practicum/jointproject/MainActivity.kt
+++ app/src/main/java/com/practicum/jointproject/MainActivity.kt
@@ -3,14 +3,21 @@ package com.practicum.jointproject
 import android.content.res.Configuration
 import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
 import com.practicum.jointproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,10 +29,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment)
                 as NavHostFragment
+        val navView = binding.navView
+        navView.getHeaderView(0).findViewById<Button>(R.id.nav_header_btn)
+            .setOnClickListener {
+                if (getString(R.string.theme) == "day")
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                if (getString(R.string.theme) == "night")
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
         val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(navView, navController)
         val builder = AppBarConfiguration.Builder(navController.graph)
+        builder.setOpenableLayout(binding.drawerLayout)
+
+
     }
+
+
 }
